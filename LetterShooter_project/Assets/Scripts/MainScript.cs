0b04c2a5422e0a0ad7a41b35710b43d91d5f3ff4
M V <mindaugas932@gmail.com>
2018-10-28 01:32:28 +0300

    Initial commit

---
+++ LetterShooter_project/Assets/Scripts/MainScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour {
+
+	public GameObject ThrowablePrefab;
+	public Transform spawnParent;
+	public float force = 10f;
+	void Start () {
+		spawnParent = this.transform;
+	}
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.G))
+		{
+			Debug.Log("G pressed.");
+			GameObject newLetter = Instantiate(ThrowablePrefab, spawnParent);
+			Vector2 newDirection = Random.insideUnitCircle;
+			newDirection.y = Mathf.Abs(newDirection.y);
+			newLetter.GetComponent<Rigidbody2D>().velocity = newDirection * force;
+		}
+	}
+}
---
+++ LetterShooter_project/Assets/Scripts/ThrowableScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("DestroyThis", 1);
+	}
+	
+	void DestroyThis () {
+		Destroy(this);
+	}
+}
